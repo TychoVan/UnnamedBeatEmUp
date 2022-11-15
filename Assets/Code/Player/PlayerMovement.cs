2a56d5386ff62788b4b8cfd3a16c1d3d25aa8de5
TychoVan <TychovanEngelenburg@gmail.com>
2022-11-15 15:30:57 +0100

    Added jumping
    Player can now do 3 jumps: jump up - jump right - jump left

--- Assets/Code/Player/PlayerMovement.cs
+++ Assets/Code/Player/PlayerMovement.cs
@@ -6,15 +6,31 @@ namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        private Vector2 inputAxis;
-        [SerializeField] private bool    canJump;
-        [SerializeField] private bool    canMove;
+        [Header("Movement")]
+        private Vector2                             inputAxis;
+        [SerializeField] private float              xMovementSpeed;
+        [SerializeField] private float              yMovementSpeed;
 
-        [SerializeField] private float xMovementSpeed;
-        [SerializeField] private float yMovementSpeed;
+        [Header("RayChecks")]
+        [SerializeField] private LayerMask          Walkable;
 
-        [SerializeField] private float jumpHeight;
-        [SerializeField] private AnimationCurve jumpspeed;
+        [SerializeField] private float              distanceToCheckTop;
+        [SerializeField] private float              distanceToCheckBottom;
+        [SerializeField] private float              distanceToCheckLeft;
+        [SerializeField] private float              distanceToCheckRight;
+
+        [Header("Jump settings")]
+        [SerializeField] private float              jumpHeight;
+        [SerializeField] private float              JumpForce;                      // speedMulitplierUp
+        [SerializeField] private float              gravity;                        // speedMulitplierDown
+        [SerializeField] private AnimationCurve     accelerationUp;
+        [SerializeField] private AnimationCurve     accelerationDown;
+        [SerializeField] private float              horizontalVelocity;
+
+
+        [Header("Debug")]
+        [SerializeField] private bool               canMove;
+        [SerializeField] private bool               canJump;
 
 
 
@@ -28,66 +44,90 @@ namespace Player
         private void Update()
         {
             #region input
-            inputAxis = new Vector2(Input.GetAxis("Horizontal"),
-                                    Input.GetAxis("Vertical"));
+            // Get walking input
+            inputAxis = new Vector2(Input.GetAxisRaw("Horizontal"),
+                                    Input.GetAxisRaw("Vertical"));
 
+            // Get jump input.
             if (canJump)
             {
-                if (Input.GetButtonDown("Jump")) Jump(inputAxis.x);
+                if (Input.GetButtonDown("Jump")) StartCoroutine(Jump(inputAxis.x));
+            }
+            #endregion
+
+
+            #region CollisionDetection
+            RaycastHit2D raycastBottom = Physics2D.Raycast(transform.position, Vector2.down, distanceToCheckBottom);
+            if (raycastBottom.collider != null)
+            {
+                if (raycastBottom.collider.CompareTag("Walkable"))
+                {
+
+                }
+            }
+            #endregion
+
+
+            #region Movement
+            if (canMove) {
+                if (inputAxis.x != 0 || inputAxis.y != 0) {
+
+                    //------- Animation Walking -------\\
+
+                    transform.position += (new Vector3(inputAxis.x * xMovementSpeed,
+                                                       inputAxis.y * yMovementSpeed,
+                                                       0) * Time.deltaTime);
+                }
             }
             #endregion
         }
 
 
-
-        private void FixedUpdate()
+        private IEnumerator Jump(float direction)
         {
-            if (canMove) {
-                if (inputAxis.x != 0 || inputAxis.y != 0) {
-                    transform.position += new Vector3(inputAxis.x * xMovementSpeed,
-                                                      inputAxis.y * yMovementSpeed,
-                                                      0) * Time.deltaTime;
-                }
-            }
-        }
-
-
-        private void Jump(float direction)
-        {
+            // Lock movement.
             canMove = false;
             canJump = false;
 
             float startHeight = transform.position.y;
-            if (direction < 0) direction = -1;
-            if (direction > 0) direction =  1;
 
-            canJump = false;
-            int limit = 1000;
-            int i = 0;
+            // Snap the direction to an absolute.
+                 if (direction < 0) direction = -1;
+            else if (direction > 0) direction =  1;
 
-            while (transform.position.y < startHeight + jumpHeight && i < limit) {
-                transform.position += new Vector3(direction,
-                                                  //jumpspeed.Evaluate(transform.position.y - startHeight / jumpHeight * 50),
-                                                  1f,
-                                                  0) * Time.deltaTime;
+            Debug.Log(direction);
 
-                Debug.Log(transform.position.y - startHeight + "/" + jumpHeight);
-                i++;
+            //------- Animation Startjump -------\\
+
+            // Move up.
+            while (transform.position.y < startHeight + jumpHeight) {
+                transform.position += (new Vector3(direction * horizontalVelocity,
+                                                   accelerationUp.Evaluate((transform.position.y - startHeight) / jumpHeight) * JumpForce,
+                                                   0) * Time.deltaTime);
+
+                // Clamp position.
+                transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, startHeight, startHeight + jumpHeight), transform.position.z);
+
+                yield return new WaitForEndOfFrame();
             }
 
-            i = 0;
 
-            while (transform.position.y > startHeight && i < limit)
+            //------- Animation EndJump -------\\
+
+            // Move down
+            while (transform.position.y > startHeight)
             {
-                transform.position -= new Vector3(direction,
-                                                  //jumpspeed.Evaluate(transform.position.y - startHeight / jumpHeight * 50),
-                                                  1f,
-                                                  0) * Time.deltaTime;
+                transform.position += (new Vector3(direction* horizontalVelocity,
+                                                   -accelerationDown.Evaluate(1 - (transform.position.y - startHeight) / jumpHeight) * gravity,
+                                                    0) * Time.deltaTime);
 
-                Debug.Log(transform.position.y - startHeight + "/" + jumpHeight);
-                i++;
+                // Clamp position.
+                transform.position  = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, startHeight, startHeight + jumpHeight), transform.position.z);
+
+                yield return new WaitForEndOfFrame();
             }
 
+            // Unlock movement.
             canJump = true;
             canMove = true;
         }
