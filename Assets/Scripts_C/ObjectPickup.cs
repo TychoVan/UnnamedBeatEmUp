d66913736a27e32581ce259f94a1c352e01bd0d6
Chivano295 <crijsdam@hotmail.com>
2022-11-28 12:08:26 +0100

    begonnen pickup

---
+++ Assets/Scripts_C/ObjectPickup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPickup : MonoBehaviour
+{
+    public GameObject player;
+    public Transform thisTransform;
+
+    public void Start()
+    {
+        thisTransform.GetComponent<Transform>();
+    }
+    public void Update()
+    {
+        
+    }
+}
