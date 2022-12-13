1416b781dba3101840a965e25039a3f3d11ab52b
TychoVan <82027614+TychoVan@users.noreply.github.com>
2022-12-13 15:15:47 +0100

    small changes attack script

--- Assets/Tycho/Code/Player/Attacks/EditorAttack.cs
+++ Assets/Tycho/Code/Player/Attacks/EditorAttack.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[System.Serializable]
-public class EditorAttack
+namespace Player
 {
-    [field: SerializeField] public string       InputButton   { get; private set; }
-    [field: SerializeField] public AttackData   AttackData    { get; private set; }
-    [field: SerializeField] public GameObject[] AttackHitboxes { get; private set; }
+    [System.Serializable]
+    public class EditorAttack
+    {
+        [field: SerializeField] public string       InputButton   { get; private set; }
+        [field: SerializeField] public AttackData   AttackData    { get; private set; }
+        [field: SerializeField] public GameObject[] AttackHitboxes { get; private set; }
+
+        [Header("DO NOT EDIT")]
+        public List<IAttack> AttackScripts;
+    }
 }
--- Assets/Tycho/Code/Player/PlayerAttack.cs
+++ Assets/Tycho/Code/Player/PlayerAttack.cs
@@ -27,6 +27,12 @@ namespace Player
         private void Start()
         {
             animator = this.gameObject.GetComponent<Animator>();
+
+            for (int i = 0; i < Attacks.Length; i++) {
+                for (int j = 0; j < Attacks[i].AttackHitboxes.Length; j++) {
+                    Attacks[i].AttackScripts.Add(Attacks[i].AttackHitboxes[j].GetComponent<IAttack>());
+                }
+            }
         }
 
 
@@ -36,18 +42,28 @@ namespace Player
             {
                 if (Input.GetButtonDown(Attacks[i].InputButton))
                 {
-                    if (Mana - Attacks[i].AttackData.ManaCost >= MinMana)
-                    {
-                        Mana = Mathf.Clamp(Mana - Attacks[i].AttackData.ManaCost, MinMana, MaxMana);
-                        OnAttack.Invoke();
-                        animator.SetInteger("AnimationState", Attacks[i].AttackData.AnimationTag);
-                        for (int j = 0; j < Attacks[i].AttackHitboxes.Length; j++)
-                        {
-                            Attacks[i].AttackHitboxes[j].SetActive(true);
-                        }
-                    }
+                    Attack(Attacks[i], Attacks[i].AttackData);
                 }
             }
+        }
+
+       
+        private void Attack(EditorAttack attack, AttackData attackData)
+        {
+            if (Mana - attackData.ManaCost >= MinMana)
+            {
+                Mana = Mathf.Clamp(Mana - attackData.ManaCost, MinMana, MaxMana);
+
+                OnAttack.Invoke();
+                animator.SetInteger("AnimationState", attackData.AnimationTag);
+
+                // Activate the attack objects.
+                for (int j = 0; j < attack.AttackHitboxes.Length; j++){
+                    attack.AttackHitboxes[j].SetActive(true);
+                    attack.AttackScripts[j].Attack(attack.AttackData.HittableTags);
+                }
+            }
+
         }
     }
 }
