1019cfd93a329cc00a05ba1627ee3a640a8037e9
TychoVan <82027614+TychoVan@users.noreply.github.com>
2022-12-19 11:26:11 +0100

    made slider more accesible

--- Assets/Tycho/Code/UI/Hud.cs
+++ Assets/Tycho/Code/UI/Hud.cs
@@ -8,58 +8,21 @@ namespace UI
 {
     public class Hud : MonoBehaviour
     {
-        [SerializeField] private Slider[] sliderElement;
+        [SerializeField] private SliderData[] sliderElements;
 
 
 
 
-        private void Start()
+        public void ChangeSliderFill(int sliderNr)
         {
-            for (int i = 0; i < sliderElement.Length; i++) {
-                Slider slider = sliderElement[i];
+            SliderData sliderData       = sliderElements[sliderNr];
+            if (!sliderData.Slider.Data.LerpingBar) {
+                 sliderData.Slider.Data = sliderData;
 
-                slider.Bar = slider.Element.transform.Find("Bar").GetComponent<Image>();
+                StartCoroutine(sliderData.Slider.LerpSlider());
             }
         }
 
 
-        public void ChangeFill(int sliderNr)
-        {
-            Slider slider       = sliderElement[sliderNr];
-
-
-            slider.LerpingBar = false;
-            if (!slider.LerpingBar) StartCoroutine(LerpSlider(slider));
-            Debug.Log("test");
-        }
-
-
-        // Smoothly change the element fill to the right value.
-        private IEnumerator LerpSlider(Slider slider)
-        {
-            Debug.Log(slider.Bar.fillAmount);
-            float timer       = 0;
-
-            // Get the fill information.
-            float startFill   = slider.Bar.fillAmount;
-            float targetFill  = (slider.FillValues.UIValue    - slider.FillValues.UIMinValue) / 
-                                (slider.FillValues.UIMaxValue - slider.FillValues.UIMinValue);
-            float currentFill = startFill;
-
-            slider.LerpingBar = true;
-
-            while (timer < 1 && slider.LerpingBar)
-            {
-                timer                 += Time.deltaTime * slider.SlideSpeed;
-                currentFill            = Mathf.Lerp(startFill, targetFill, timer);
-                slider.Bar.fillAmount  = currentFill;
-
-                yield return new WaitForEndOfFrame();
-            }
-
-            slider.LerpingBar = false;
-
-            yield return null;
-        }
     }
 }
