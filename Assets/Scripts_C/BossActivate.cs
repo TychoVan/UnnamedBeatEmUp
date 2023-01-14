db0d29ca9faa11520541189a83e5bc8861971f6e
Chivano295 <crijsdam@hotmail.com>
2023-01-14 14:32:34 +0100

    voor tycho

--- Assets/Scripts_C/BossActivate.cs
+++ Assets/Scripts_C/BossActivate.cs
@@ -6,11 +6,12 @@ public class BossActivate : MonoBehaviour
 {
 
     public BossBrain brain;
+    public GameObject bossUI;
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-
+            bossUI.SetActive(true);
             brain.allowedAttack = true;
         }
     }
